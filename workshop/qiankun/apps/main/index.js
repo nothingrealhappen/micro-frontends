f2ccd3ba783a74e90384874d3fd1e869b7bc96bc
Yaodong Li <mail@liyaodong.com>
2020-04-20 11:45:51 +0800

    Add restricted style isolation

--- workshop/qiankun/apps/main/index.js
+++ workshop/qiankun/apps/main/index.js
@@ -1,24 +1,25 @@
 import { registerMicroApps, start } from 'qiankun';
 
 registerMicroApps([
-  {
-    name: 'nav',
-    entry: '//localhost:8001/',
-    container: '#nav',
-    activeRule: () => true,
-  },
-  {
-    name: 'login',
-    entry: '//localhost:8002/',
-    container: '#main',
-    activeRule: '/login',
-  },
-  {
-    name: 'home', // app name registered
-    entry: '//localhost:8003/',
-    container: '#main',
-    activeRule: '/home',
-  },
+    {
+        name: 'nav',
+        entry: '//localhost:8001/',
+        container: '#nav',
+        activeRule: () => true,
+    },
+    {
+        name: 'login',
+        entry: '//localhost:8002/',
+        container: '#main',
+        activeRule: '/login',
+    },
+    {
+        name: 'home', // app name registered
+        entry: '//localhost:8003/',
+        container: '#main',
+        activeRule: '/home',
+    },
 ]);
 
 start({ singular: false });
+// start({ singular: false, sandbox: { strictStyleIsolation: true } });
