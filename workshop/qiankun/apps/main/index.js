6c94a437eb53b5b0039f6984206158fcdeca9333
Yaodong Li <mail@liyaodong.com>
2020-04-20 11:24:20 +0800

    Config correct http port for each app

--- workshop/qiankun/apps/main/index.js
+++ workshop/qiankun/apps/main/index.js
@@ -3,21 +3,21 @@ import { registerMicroApps, start } from 'qiankun';
 registerMicroApps([
   {
     name: 'nav',
-    entry: '//localhost:8000/',
+    entry: '//localhost:8001/',
     container: '#nav',
     activeRule: () => true,
   },
   {
-    name: 'home', // app name registered
-    entry: '//localhost:8001/',
-    container: '#main',
-    activeRule: '/home',
-  },
-  {
-    name: 'dashboard',
+    name: 'login',
     entry: '//localhost:8002/',
     container: '#main',
-    activeRule: '/dashboard',
+    activeRule: '/login',
+  },
+  {
+    name: 'home', // app name registered
+    entry: '//localhost:8003/',
+    container: '#main',
+    activeRule: '/home',
   },
 ]);
 
